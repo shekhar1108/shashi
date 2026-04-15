cbc750605b671749029e74307c435166d643147a
Shashi shekhar <shashishekhar9470@gmail.com>
2026-04-15 13:44:33 +0530

    Implement heap sort algorithm in Heap.cpp

---
+++ Heap.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std;
+void heapify(int arr[], int n, int i) {
+    int largest = i;
+    int left = 2 * i + 1; // left child index
+    int right = 2 * i + 2; // right child index
+
+    // If left child is larger than root
+    if (left < n && arr[left] > arr[largest])
+        largest = left;
+
+    // If right child is larger than largest so far
+    if (right < n && arr[right] > arr[largest])
+        largest = right;
+
+    // If largest is not root
+    if (largest != i) {
+        swap(arr[i], arr[largest]); // Swap root with largest
+
+        // Recursively heapify the affected sub-tree
+        heapify(arr, n, largest);
+    }
+}
+
+void heapSort(int arr[], int n) {
+    // Build heap (rearrange array)
+    for (int i = n / 2 - 1; i >= 0; i--)
+        heapify(arr, n, i);
+
+    // One by one extract elements from heap
+    for (int i = n - 1; i > 0; i--) {
+        swap(arr[0], arr[i]); // Move current root to end
+        heapify(arr, i, 0); // Call heapify on the reduced heap
+    }
+}
+
+int main() {
+    int arr[] = {12, 11, 13, 5, 6, 7};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    heapSort(arr, n);
+
+    cout << "Sorted array is: ";
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    return 0;
+}
